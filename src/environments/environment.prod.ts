c75b8981c73917d25c47dd085022310867e7ac61
Bury Vitali <vitalisbayer@gmail.com>
2021-06-29 22:20:09 +0300

    feat: set firebase, update login method, add login response interface

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyAKirS0wwNfY18ikmtNbQOLMAWiW2B75ew'
 };
