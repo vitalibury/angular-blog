e05c6d7844c44f4a67b2cd0bd4ce9380c22064c5
Bury Vitali <vitalisbayer@gmail.com>
2021-07-12 21:24:53 +0300

    feat: implement post page component

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Post } from '../shared/intefaces';
+import { PostService } from '../shared/services/post.service';
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  post$: Observable<Post>;
+
+  constructor(private route: ActivatedRoute, private postService: PostService) { }
 
   ngOnInit(): void {
+    this.post$ = this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postService.getById(params['id']);
+      }))
   }
 
 }
